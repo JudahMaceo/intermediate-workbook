bb28512aa68ed3c766194d85bb082862f1e372d2
Judah M. Ritterman <judah29@gmail.com>
2016-07-29 14:25:32 -0500

    add judah pig latin practice

--- apps/01judahPigLatin.js
+++ apps/01judahPigLatin.js
@@ -3,8 +3,8 @@ var vowels = ['A', 'E', 'I', 'O', 'U', 'Y'];
 
 function pigLatin(word){
   for(j = 0; j < vowels.length; j++){
-    if(word[0].toLowerCase()=== vowels[j].toLowerCase()){
-      return word + "yay";
+    if(word[0].toLowerCase() === vowels[j].toLowerCase()){
+      return word.toLowerCase() + 'yay';
     }
     else{
       var firstPartWord = firstPart(word);
@@ -27,4 +27,11 @@ function firstPart(word){
   }
 }
 
-console.log(pigLatin('pregghead'));
+console.log(pigLatin('car'), 'arcay');
+console.log(pigLatin('dog'), 'ogday');
+console.log(pigLatin('create'), 'eatecray');
+console.log(pigLatin('valley'), 'alleyvay');
+console.log(pigLatin('egg'), 'eggyay');
+// console.log(pigLatin('emission'), 'emissionyay');
+// console.log(pigLatin('heLlo'), 'ellohay');
+// console.log(pigLatin('RoCkEt'), 'ocketray');
